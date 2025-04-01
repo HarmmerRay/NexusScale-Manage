dce960c313152460defb61055349e2c10cfd0b57
HammerRay <zhao131804@gmail.com>
2025-04-01 22:46:11 +0800

    1

--- Test.java
+++ Test.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
 import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test {
     public int lengthOfLongestSubstring(String s) {
         Map<Character, Integer> map = new HashMap<>();
+        Queue<Character> queue = new LinkedList<>();
         return 0;
     }
     public static void main(String[] args) {
         System.out.println("Hello World");
-
+        Queue<Character> queue = new ArrayDeque<>(2);
+        System.out.println(queue.add('a'));
+        System.out.println(queue.add('b'));
+        System.out.println(queue.add('c'));
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
     }
 }
